0f489bc1c75925925262cac39bfd176f6c197eae
iamamirshabir <iamamirshabir@gmail.com>
2021-03-17 12:38:33 +0500

    Second Commit

--- src/main/java/com/pioneer/dips/symptom/controller/symptomController.java
+++ src/main/java/com/pioneer/dips/symptom/controller/symptomController.java
@@ -5,6 +5,9 @@ import java.util.stream.Collectors;
 
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.http.ResponseEntity;
+
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,44 +18,50 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pioneer.dips.symptom.model.Symptom;
+import com.pioneer.dips.symptom.model.SymptomModelAssembler;
 import com.pioneer.dips.symptom.repository.symptomRepository;
 
 @RestController
 public class symptomController {
 	
 	private final symptomRepository repository;
+	private final SymptomModelAssembler assembler; 
 	
-	  symptomController(symptomRepository repository) {
+	  symptomController(symptomRepository repository, SymptomModelAssembler assembler) {
 		    this.repository = repository;
+		    this.assembler = assembler;
 		  }
 	  
 	  @GetMapping("/symptom")
+	public
 	  CollectionModel<EntityModel<Symptom>> all(){
-		  List<EntityModel<Symptom>> symptoms = repository.findAll().stream().map(symptom -> EntityModel.of(symptom, 
-				  linkTo(methodOn(symptomController.class).one(symptom.getSYMPTOM_ID())).withSelfRel(),
-				  linkTo(methodOn(symptomController.class).all()).withRel("symptom")))
+		  List<EntityModel<Symptom>> symptoms = repository.findAll().stream()
+				  .map(assembler :: toModel)
 				  .collect(Collectors.toList());
 		  return CollectionModel.of(symptoms,
 				  linkTo(methodOn(symptomController.class).all()).withSelfRel());
 	  }
 	  
 	  @PostMapping("/symptom")
-	  Symptom newSymptom(@RequestBody Symptom newSymptom ) {
-		  return repository.save(newSymptom);
-	   }
+	  ResponseEntity<?> newSymptom(@RequestBody Symptom newSymptom ) {
+		EntityModel<Symptom> symptom = assembler.toModel(repository.save(newSymptom));
+		  return ResponseEntity
+				  .created(symptom.getRequiredLink(IanaLinkRelations.SELF).toUri())
+				  .body(symptom);
+	   
+	  }
 	  
 	  @GetMapping("/symptom/{id}")
+	public
 	  EntityModel<Symptom> one(@PathVariable Long id) {
 		  Symptom symptom = repository.findById(id)
 				  .orElseThrow(() -> new SymptomNotFoundException(id));
-		  return EntityModel.of(symptom, 
-				  linkTo(methodOn(symptomController.class).one(id)).withSelfRel(),
-				  linkTo(methodOn(symptomController.class).all()).withRel("symptom"));
+		  return assembler.toModel(symptom);
 	  }
 	  
 	  @PutMapping("symptom/{id}")
-	  Symptom replaceSymptom(@RequestBody Symptom newSymptom, @PathVariable Long id) {
-		  return repository.findById(id)
+	  ResponseEntity<?> replaceSymptom(@RequestBody Symptom newSymptom, @PathVariable Long id) {
+		  Symptom updatedSymptom = repository.findById(id)
 				  .map(symptom ->{
 					  symptom.setSYMPTOM_NAME(newSymptom.getSYMPTOM_NAME());
 					  symptom.setSYMPTOM_CATEGORY_ID(newSymptom.getSYMPTOM_CATEGORY_ID());
@@ -62,7 +71,12 @@ public class symptomController {
 					  newSymptom.setSYMPTOM_ID(id);
 					  return repository.save(newSymptom);
 				  });
-  
+		  EntityModel<Symptom> symptoms = assembler.toModel(updatedSymptom);
+		  
+		  return ResponseEntity
+				  .created(symptoms.getRequiredLink(IanaLinkRelations.SELF).toUri())
+				  .body(symptoms);
+					 
 	  }
 	  
 	  @DeleteMapping("/symptom/{id}")
