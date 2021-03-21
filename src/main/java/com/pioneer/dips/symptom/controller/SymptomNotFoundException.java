24c82edd1ce0974b1997f69478d24e81d7f23390
iamamirshabir <iamamirshabir@gmail.com>
2021-03-21 12:58:38 +0500

    set variable port

--- src/main/java/com/pioneer/dips/symptom/controller/SymptomNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.pioneer.dips.symptom.controller;
-
-public class SymptomNotFoundException extends RuntimeException {
-
-	public SymptomNotFoundException(Long id){
-		super("Could not find symptom" + id);
-	}
-}
